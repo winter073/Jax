be8f8701ab698484fc5c24ebd1e37e513f4ec883
Kenny <khuynh2@student.fitchburgstate.edu>
2022-04-04 15:00:30 -0400

    Created three tutorial levels for movement, camera rotation, and climbing. Also created a script to change to the next scene (only works from movement tutorial to skatepark. Flag Goal object is created, name of skatepark is renamed to "Skatepark_V1".

---
+++ Assets/Scripts/Scene_Switch.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Switch : MonoBehaviour
+{
+    // Switches to the next level/scene
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
