65290f6f2d7f970412ed40af325c551de3a9b7dc
Kenny <khuynh2@student.fitchburgstate.edu>
2022-05-02 14:52:42 -0400

    Created a new tutorial for camera movement, refined the levels and organized the hierarchy, and edited a script for loading the next scene

--- Assets/Scripts/Scene_Switch.cs
+++ Assets/Scripts/Scene_Switch.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Script for Jax
+//Written by Kenny Huynh
+
 public class Scene_Switch : MonoBehaviour
 {
+    private int sceneInt;
+
     // Switches to the next level/scene
     void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene("Skatepark_V1");
+        changeScene();
+        if (sceneInt > 1)
+        {
+            SceneManager.LoadScene("Skatepark_V1");
+        }
+        else
+        {
+            SceneManager.LoadScene("Tutorial_Camera");
+        }
+    }
+
+    public void changeScene()
+    {
+        sceneInt++;
     }
 }
