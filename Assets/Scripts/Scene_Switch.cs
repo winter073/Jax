e869bd225cd903fbb696dadc7327b0d81eb0e009
Kenny <khuynh2@student.fitchburgstate.edu>
2022-05-05 15:43:18 -0400

    Refined the first tutorial level

--- Assets/Scripts/Scene_Switch.cs
+++ Assets/Scripts/Scene_Switch.cs
@@ -8,24 +8,9 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Switch : MonoBehaviour
 {
-    private int sceneInt;
-
     // Switches to the next level/scene
     void OnTriggerEnter(Collider other)
     {
-        changeScene();
-        if (sceneInt > 1)
-        {
-            SceneManager.LoadScene("Skatepark_V1");
-        }
-        else
-        {
-            SceneManager.LoadScene("Tutorial_Camera");
-        }
-    }
-
-    public void changeScene()
-    {
-        sceneInt++;
+        SceneManager.LoadScene("Tutorial_Camera");
     }
 }
